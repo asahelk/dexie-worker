6d98b656b1cc9d1d69ab250ae4e2cf3a1690f157
Asahel <abraham_zm22@hotmail.com>
2025-04-02 13:32:42 -0500

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-export { default as getWebWorkerDB } from './getWebWorkerDB';
+export { getWebWorkerDB } from './getWebWorkerDB';
 export { liveQuery } from './liveQuery';
-export { default as useLiveQuery } from './useLiveQuery';
+export { useLiveQuery } from './useLiveQuery';
 export type * from './types/common.ts';
